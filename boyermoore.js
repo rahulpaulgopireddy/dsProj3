7380c898082639ccfe35e3f36974053582657545
rahul paul gopi reddy <rahulpaulgopireddy@rahuls-MacBook-Pro.local>
2022-12-02 02:09:00 -0500

    boyermoorealgo done

---
+++ boyermoore.js
@@ -0,0 +1,62 @@
+import inputData from "./sample.json" assert { type: "json" };
+
+let range = 200;
+inputData.forEach((element, index) => {
+  var startTime = performance.now();
+  BMSearch(element.stringText, element.pattern);
+  var endTime = performance.now();
+  console.log(`/n ${endTime - startTime}`);
+});
+
+function FailureTableHeuristic(stringtext, size, failureChar) {
+  // initial all the values with -1
+
+  for (let index = 0; index < range; index++) {
+    failureChar[index] = -1;
+  }
+
+  //fill the values that are indexes of occrurance
+  for (let index = 0; index < size; index++) {
+    failureChar[stringtext[index].charCodeAt(0)] = index;
+  }
+}
+
+function maxValue(a, b) {
+  return a > b ? a : b;
+}
+
+function BMSearch(stringText, pattern) {
+  let CompCount = 0;
+  let patLen = pattern.length;
+  let strLen = stringText.length;
+
+  let failureChar = new Array(range);
+
+  FailureTableHeuristic(pattern, patLen, failureChar);
+  console.log(failureChar);
+
+  let shiftNumber = 0;
+
+  while (shiftNumber <= strLen - patLen) {
+    let j = patLen - 1;
+
+    while (j >= 0 && pattern[j] == stringText[shiftNumber + j]) {
+      CompCount++;
+      console.log(CompCount);
+      j--;
+    }
+
+    if (j < 0) {
+      //   console.log(" At Shift " + shiftNumber + "Pattern occures");
+      shiftNumber +=
+        shiftNumber + patLen < strLen
+          ? patLen - failureChar[stringText[shiftNumber + patLen].charCodeAt(0)]
+          : 1;
+    } else {
+      shiftNumber += maxValue(
+        1,
+        j - failureChar[stringText[shiftNumber + j].charCodeAt(0)]
+      );
+    }
+  }
+}
--- brute-force.js
+++ brute-force.js
@@ -11,20 +11,13 @@ function stringMatch(string, pattern) {
   let patLen = pattern.length;
   let stringLen = string.length;
   let compareCount = 0;
-  /* A loop to slide pat one by one */
   for (let _patIndex = 0; _patIndex <= stringLen - patLen; _patIndex++) {
     let _strindex;
-
-    /* For current index i, check for pattern
-        match */
     for (_strindex = 0; _strindex < patLen; _strindex++)
       if (string[_patIndex + _strindex] != pattern[_strindex]) {
         compareCount += 1;
         break;
       }
-    // compareCount += 1;
-
-    // if pat[0...M-1] = txt[i, i+1, ...i+M-1]
     if (_strindex == patLen)
       console.log(
         "Pattern found at index " +
