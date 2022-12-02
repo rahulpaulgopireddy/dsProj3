bf177f1dbd3a682a2f0740e80df8ba0f2f1b4d01
rahul paul gopi reddy <rahulpaulgopireddy@rahuls-MacBook-Pro.local>
2022-12-02 02:19:49 -0500

    run time added

--- boyermoore.js
+++ boyermoore.js
@@ -1,11 +1,15 @@
-import inputData from "./sample.json" assert { type: "json" };
+import inputData from "./inputdata.json" assert { type: "json" };
 
 let range = 200;
 inputData.forEach((element, index) => {
   var startTime = performance.now();
   BMSearch(element.stringText, element.pattern);
   var endTime = performance.now();
-  console.log(`/n ${endTime - startTime}`);
+  console.log(
+    ` Execution time to find "${element.pattern}" in string " ${
+      element.stringText
+    } "   ${endTime - startTime} Milliseconds`
+  );
 });
 
 function FailureTableHeuristic(stringtext, size, failureChar) {
@@ -29,25 +33,17 @@ function BMSearch(stringText, pattern) {
   let CompCount = 0;
   let patLen = pattern.length;
   let strLen = stringText.length;
-
   let failureChar = new Array(range);
-
   FailureTableHeuristic(pattern, patLen, failureChar);
-  console.log(failureChar);
-
   let shiftNumber = 0;
-
   while (shiftNumber <= strLen - patLen) {
     let j = patLen - 1;
-
     while (j >= 0 && pattern[j] == stringText[shiftNumber + j]) {
-      CompCount++;
-      console.log(CompCount);
       j--;
     }
 
     if (j < 0) {
-      //   console.log(" At Shift " + shiftNumber + "Pattern occures");
+      console.log(" At Shift " + shiftNumber + "Pattern occures");
       shiftNumber +=
         shiftNumber + patLen < strLen
           ? patLen - failureChar[stringText[shiftNumber + patLen].charCodeAt(0)]
